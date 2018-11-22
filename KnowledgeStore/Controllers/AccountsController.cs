3ff93fe8024824349c8b9eab421dfdc8e67b53e6
QUOCVINH-PC\Quốc Vinh <quocvinhnguyen97@gmail.com>
2018-11-22 19:17:06 +0700

    Vinh commit
    Thêm loạc sản phẩm trang chủ

--- KnowledgeStore/Controllers/AccountsController.cs
+++ KnowledgeStore/Controllers/AccountsController.cs
@@ -218,5 +218,40 @@ namespace KnowledgeStore.Controllers
             base.Dispose(disposing);
 
         }
+
+
+
+
+
+        public ActionResult CustomerInfo(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var result = dao.Login(model.Email, Encryptor.SHA256Encrypt(model.Password));
+                if (result == 1)
+                {
+                    var userSession = new UserLogin();
+                    userSession.UserName = db.Customers.Where(m => m.Email == model.Email).Select(m => m.HoTen).FirstOrDefault();
+                    userSession.Email = model.Email;
+                    Session[CommonConstants.USER_SESSION] = null;
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Home");
+                }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản bị khóa.");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại.");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "Mật khẩu không đúng.");
+                }
+            }
+            return View(model);
+        }
     }
 }
--- KnowledgeStore/Controllers/HomeController.cs
+++ KnowledgeStore/Controllers/HomeController.cs
@@ -16,18 +16,88 @@ namespace KnowledgeStore.Controllers
             return View(db.Saches.ToList());
         }
 
-        public JsonResult ViewMore(int xPosition, int height)
+        public JsonResult ViewMore(int height, int xPosition)
         {
             var listSach = new List<BookHomeVM>();
-            var list = db.Saches.ToList();
-            for (int i = 0; i <= xPosition + 8; i++)
+            var list = db.Saches.OrderByDescending(p => p.SachID).ToList();
+
+            for (int i = 0; i < xPosition + 8; i++)
             {
                 BookHomeVM bookHomeVM = new BookHomeVM() { SachID = list[i].SachID, TenSach = list[i].TenSach, TenTheLoai = list[i].TheLoai.TenTheLoai, GiaTien = list[i].GiaTien, GiaKhuyenMai = list[i].GiaKhuyenMai, MoTa = list[i].MoTa, TrangThai = list[i].TrangThai, SoLuong = list[i].SoLuong, TenCuaHang = list[i].Merchant.TenCuaHang };
                 listSach.Add(bookHomeVM);
             }
-            return Json(new { data = listSach, position = xPosition + 8, heightStyle = height + 610, top = 0 }, JsonRequestBehavior.AllowGet);
+            return Json(new { data = listSach, heightStyle = height + 610, position = xPosition + 8, top = 0 }, JsonRequestBehavior.AllowGet);
+
         }
 
+        public JsonResult ViewNew(int height, int xPosition)
+        {
+            var listSach = new List<BookHomeVM>();
+            var list = db.Saches.OrderByDescending(p => p.NgayXuatBan).ToList();
+
+            for (int i = 0; i < xPosition + 8; i++)
+            {
+                BookHomeVM bookHomeVM = new BookHomeVM() { SachID = list[i].SachID, TenSach = list[i].TenSach, TenTheLoai = list[i].TheLoai.TenTheLoai, GiaTien = list[i].GiaTien, GiaKhuyenMai = list[i].GiaKhuyenMai, MoTa = list[i].MoTa, TrangThai = list[i].TrangThai, SoLuong = list[i].SoLuong, TenCuaHang = list[i].Merchant.TenCuaHang };
+                listSach.Add(bookHomeVM);
+            }
+            return Json(new { data = listSach, heightStyle = height + 610, position = xPosition + 8, top = 0 }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult ViewBestSales(int height, int xPosition)
+        {
+            var listSach = new List<BookHomeVM>();
+            var list = db.Saches.Where(p => p.GiaKhuyenMai != null)
+                .OrderBy(p => p.GiaKhuyenMai).ToList();
+
+            for (int i = 0; i < xPosition + 8; i++)
+            {
+                if (i > list.Count - 1)
+                    break;
+                BookHomeVM bookHomeVM = new BookHomeVM() { SachID = list[i].SachID, TenSach = list[i].TenSach, TenTheLoai = list[i].TheLoai.TenTheLoai, GiaTien = list[i].GiaTien, GiaKhuyenMai = list[i].GiaKhuyenMai, MoTa = list[i].MoTa, TrangThai = list[i].TrangThai, SoLuong = list[i].SoLuong, TenCuaHang = list[i].Merchant.TenCuaHang };
+                listSach.Add(bookHomeVM);
+
+
+            }
+            return Json(new { data = listSach, heightStyle = height + 610, position = xPosition + 8, top = 0 }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult ViewLowPrice(int height, int xPosition)
+        {
+            var listSach = new List<BookHomeVM>();
+            var list = db.Saches
+                .OrderBy(p => p.GiaTien).ToList();
+
+            for (int i = 0; i < xPosition + 8; i++)
+            {
+
+                BookHomeVM bookHomeVM = new BookHomeVM() { SachID = list[i].SachID, TenSach = list[i].TenSach, TenTheLoai = list[i].TheLoai.TenTheLoai, GiaTien = list[i].GiaTien, GiaKhuyenMai = list[i].GiaKhuyenMai, MoTa = list[i].MoTa, TrangThai = list[i].TrangThai, SoLuong = list[i].SoLuong, TenCuaHang = list[i].Merchant.TenCuaHang };
+                listSach.Add(bookHomeVM);
+
+
+            }
+            return Json(new { data = listSach, heightStyle = height + 610, position = xPosition + 8, top = 0 }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult ViewHighPrice(int height, int xPosition)
+        {
+            var listSach = new List<BookHomeVM>();
+            var list = db.Saches
+                .OrderByDescending(p => p.GiaTien).ToList();
+
+            for (int i = 0; i < xPosition + 8; i++)
+            {
+
+                BookHomeVM bookHomeVM = new BookHomeVM() { SachID = list[i].SachID, TenSach = list[i].TenSach, TenTheLoai = list[i].TheLoai.TenTheLoai, GiaTien = list[i].GiaTien, GiaKhuyenMai = list[i].GiaKhuyenMai, MoTa = list[i].MoTa, TrangThai = list[i].TrangThai, SoLuong = list[i].SoLuong, TenCuaHang = list[i].Merchant.TenCuaHang };
+                listSach.Add(bookHomeVM);
+
+
+            }
+            return Json(new { data = listSach, heightStyle = height + 610, position = xPosition + 8, top = 0 }, JsonRequestBehavior.AllowGet);
+
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -41,5 +111,6 @@ namespace KnowledgeStore.Controllers
 
             return View();
         }
+
     }
 }
