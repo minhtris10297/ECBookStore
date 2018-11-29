5398cea060b960965655b332b2e47477db878ac4
Nguyen Hieu Trung <nguyenhieutrung.07@gmail.com>
2018-11-29 07:46:48 +0700

    Trung commit
    trung

---
+++ KnowledgeStore/Areas/Merchant/Controllers/OrderManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeStore.Areas.Merchant.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        // GET: Merchant/OrderManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
