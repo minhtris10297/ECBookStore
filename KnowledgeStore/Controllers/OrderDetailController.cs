9b9d145f2d31e06e9320c165a31b1193415d9e88
Nguyen Hieu Trung <nguyenhieutrung.07@gmail.com>
2018-12-04 14:19:14 +0700

    Trung commit
    Trung

---
+++ KnowledgeStore/Controllers/OrderDetailController.cs
@@ -0,0 +1,30 @@
+﻿using KnowledgeStore.Common;
+using Model.EntityFramework;
+using Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeStore.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        KnowledgeStoreEntities db = new KnowledgeStoreEntities();
+        // GET: OrderDetail
+        public ActionResult Index()
+        {
+            var sessionUser = (UserLogin)Session[CommonConstants.USER_SESSION];
+
+            if (sessionUser == null)
+            {
+                return RedirectToAction("Login", "Accounts");
+            }
+            var cusId = db.Customers.Where(m => m.Email == sessionUser.Email).Select(m=>m.CustomerID).FirstOrDefault();
+            var listOrder = db.ChiTietDonHangs.Where(m => m.DonHang.CustomerID == cusId).ToList();
+            
+            return View(listOrder);
+        }
+    }
+}
