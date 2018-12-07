0ddec568dbb96f5b1083b02984019dd9e9d3d05a
nguyenhieutrung97 <30740080+nguyenhieutrung97@users.noreply.github.com>
2018-12-07 15:58:19 +0700

    Trung Commit
    Trung

--- KnowledgeStore/Areas/MerchantArea/Controllers/BookManagerController.cs
+++ KnowledgeStore/Areas/MerchantArea/Controllers/BookManagerController.cs
@@ -7,6 +7,8 @@ using System.Web.Mvc;
 using PagedList;
 using KnowledgeStore.Common;
 using Model.ViewModel;
+using System.Data.Entity;
+using System.Net;
 
 namespace KnowledgeStore.Areas.MerchantArea.Controllers
 {
@@ -18,9 +20,7 @@ namespace KnowledgeStore.Areas.MerchantArea.Controllers
         {
             ViewBag.dropDownNXB = new SelectList(db.NhaXuatBans, "NhaXuatBanID", "TenNXB");
             ViewBag.dropDownTheLoai = new SelectList(db.TheLoais, "TheLoaiID", "TenTheLoai");
-            ViewBag.listTheLoai = db.TheLoais;
-            ViewBag.listBia = db.LoaiBias;
-            ViewBag.listNXB = db.NhaXuatBans;
+            ViewBag.GiaNangTin = db.GiaTiens.Where(m => m.GiaTienID == 1).Select(m => m.TyGia).FirstOrDefault();
 
             var sessionUser = (UserLogin)Session[CommonConstants.USERMERCHANT_SESSION];
             if (sessionUser == null)
@@ -36,26 +36,24 @@ namespace KnowledgeStore.Areas.MerchantArea.Controllers
         {
             ViewBag.dropDownNXB = new SelectList(db.NhaXuatBans, "NhaXuatBanID", "TenNXB");
             ViewBag.dropDownTheLoai = new SelectList(db.TheLoais, "TheLoaiID", "TenTheLoai");
-            ViewBag.listTheLoai = db.TheLoais;
-            ViewBag.listBia = db.LoaiBias;
-            ViewBag.listNXB = db.NhaXuatBans;
+            ViewBag.GiaNangTin = db.GiaTiens.Where(m => m.GiaTienID == 1).Select(m => m.TyGia).FirstOrDefault();
 
             var sessionUser = (UserLogin)Session[CommonConstants.USERMERCHANT_SESSION];
             var id = db.Merchants.Where(m => m.Email == sessionUser.Email).Select(m => m.MerchantID).FirstOrDefault();
 
-            var listSach = db.Saches.Where(m => m.MerchantID == id).OrderByDescending(m => m.LichSuNangTins.Max(n=>n.NgayNang)).ToList();
+            var listSach = db.Saches.Where(m => m.MerchantID == id).OrderByDescending(m => m.LichSuNangTins.Max(n => n.NgayNang)).ToList();
             if (ModelState.IsValid)
             {
 
                 if (dropDownNXB != null)
                 {
                     //listCTDH = db.ChiTietDonHangs.Where(m => m.MerchantID == id&m.DonHang.NgayDat==searchTime).OrderByDescending(m => m.DonHang.NgayDat);
-                    listSach = listSach.Where(m => m.NhaXuatBanID==dropDownNXB).ToList();
+                    listSach = listSach.Where(m => m.NhaXuatBanID == dropDownNXB).ToList();
                 }
                 if (dropDownTheLoai != null)
                 {
                     //listCTDH= db.ChiTietDonHangs.Where(m => m.MerchantID == id&m.TinhTrangDonHangID==searchId).OrderByDescending(m => m.DonHang.NgayDat);
-                    listSach = listSach.Where(m => m.TheLoaiID==dropDownTheLoai).ToList();
+                    listSach = listSach.Where(m => m.TheLoaiID == dropDownTheLoai).ToList();
                 }
                 if (nameSach != null)
                 {
@@ -65,20 +63,49 @@ namespace KnowledgeStore.Areas.MerchantArea.Controllers
                 if (searchId != null)
                 {
                     //listCTDH = db.ChiTietDonHangs.Where(m => m.MerchantID == id & m.TinhTrangDonHangID == DropdownStatus).OrderByDescending(m => m.DonHang.NgayDat);
-                    listSach = listSach.Where(m => m.SachID== searchId).ToList();
+                    listSach = listSach.Where(m => m.SachID == searchId).ToList();
                 }
             }
             return View(listSach);
 
         }
 
-        [HttpPost]
-        public ActionResult Index( int id,Sach sach)
+        public ActionResult Edit(int? id)
         {
-            var book = db.Saches.Find(id);
-            book = sach;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sach sach = db.Saches.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LoaiBiaID = new SelectList(db.LoaiBias, "LoaiBiaID", "LoaiBia1", sach.LoaiBiaID);
+            ViewBag.MerchantID = new SelectList(db.Merchants, "MerchantID", "Email", sach.MerchantID);
+            ViewBag.NhaXuatBanID = new SelectList(db.NhaXuatBans, "NhaXuatBanID", "TenNXB", sach.NhaXuatBanID);
+            ViewBag.TheLoaiID = new SelectList(db.TheLoais, "TheLoaiID", "TenTheLoai", sach.TheLoaiID);
+            return View(sach);
+        }
+
+        // POST: AdminArea/Saches/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SachID,TenSach,TacGia,NhaXuatBanID,NgayXuatBan,SoTrang,LoaiBiaID,MerchantID,TrangThai,GiaTien,GiaKhuyenMai,MoTa,SoLuong,TheLoaiID")] Sach sach)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sach).State = EntityState.Modified;
                 db.SaveChanges();
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            ViewBag.LoaiBiaID = new SelectList(db.LoaiBias, "LoaiBiaID", "LoaiBia1", sach.LoaiBiaID);
+            ViewBag.MerchantID = new SelectList(db.Merchants, "MerchantID", "Email", sach.MerchantID);
+            ViewBag.NhaXuatBanID = new SelectList(db.NhaXuatBans, "NhaXuatBanID", "TenNXB", sach.NhaXuatBanID);
+            ViewBag.TheLoaiID = new SelectList(db.TheLoais, "TheLoaiID", "TenTheLoai", sach.TheLoaiID);
+            return View(sach);
         }
     }
 
---
+++ KnowledgeStore/Areas/MerchantArea/Controllers/MCHomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Model.ViewModel;
+using KnowledgeStore.Common;
+using Model.EntityFramework;
+
+namespace KnowledgeStore.Areas.MerchantArea.Controllers
+{
+    public class MCHomeController : Controller
+    {
+        KnowledgeStoreEntities db = new KnowledgeStoreEntities();
+        // GET: MerchantArea/MCHome
+        public ActionResult Index(string id, int? page)
+        {
+            var sessionUser = (UserLogin)Session[CommonConstants.USERMERCHANT_SESSION];
+            if (sessionUser == null)
+            {
+                return RedirectToAction("Login", "AccountsMerchant");
+            }var merchantID = db.Merchants.Where(m => m.Email == sessionUser.Email).Select(m => m.MerchantID).FirstOrDefault();
+
+            var listSach = db.Saches.Where(m=>m.MerchantID== merchantID).ToList();
+            if (id != null)
+            {
+                if (id == "SachGiamGia")
+                {
+                    listSach = db.Saches.OrderByDescending(m => (m.GiaKhuyenMai / m.GiaTien)).Where(m => m.TrangThai == true).ToList();
+                }
+                else if (id == "SachMoiPhatHanh")
+                {
+                    listSach = db.Saches.OrderByDescending(m => m.NgayXuatBan).Where(m => m.TrangThai == true).ToList();
+                }
+                else if (id == "SachBanChay")
+                {
+                    listSach = db.Saches.OrderByDescending(m => m.LichSuNangTins.Max(n => n.NgayNang)).Where(m => m.TrangThai == true).ToList();
+                }
+            }
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            return View(listSach.ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
