6876d6eb9e7dc3d86d99ce60870fb9eadec6d110
QUOCVINH-PC\Quốc Vinh <quocvinhnguyen97@gmail.com>
2018-12-09 19:00:55 +0700

    jkdfbuwbdf`
    jdwfauwe

---
+++ KnowledgeStore/Areas/AdminArea/Controllers/NguoiDungController.cs
@@ -0,0 +1,21 @@
+﻿using Model.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeStore.Areas.AdminArea.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        KnowledgeStoreEntities db = new KnowledgeStoreEntities();
+        // GET: AdminArea/NguoiDung
+        public ActionResult Index()
+        {
+            var listCUS = db.Customers.ToList();
+
+            return View(listCUS);
+        }
+    }
+}
