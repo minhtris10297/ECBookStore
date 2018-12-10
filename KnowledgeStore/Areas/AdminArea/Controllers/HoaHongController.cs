201b00a566fc111a49a075597610fbf543e6855d
TRARESIVES\User <trisnm102@gmail.com>
2018-12-10 16:18:28 +0700

    16:16 10/12/2018

---
+++ KnowledgeStore/Areas/AdminArea/Controllers/HoaHongController.cs
@@ -0,0 +1,19 @@
+﻿using Model.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeStore.Areas.AdminArea.Controllers
+{
+    public class HoaHongController : Controller
+    {
+        KnowledgeStoreEntities db = new KnowledgeStoreEntities();
+        // GET: AdminArea/HoaHong
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KnowledgeStore/Areas/AdminArea/Controllers/KipXuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeStore.Areas.AdminArea.Controllers
+{
+    public class KipXuController : Controller
+    {
+        // GET: AdminArea/KipXu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
