436ada8cbfaf228157a89049f954a192e581f808
minhtris10297 <32105394+minhtris10297@users.noreply.github.com>
2018-12-15 15:24:32 +0700

    CC Vinh Nâu

--- KnowledgeStore/Areas/AdminArea/Controllers/DuLieuController.cs
+++ KnowledgeStore/Areas/AdminArea/Controllers/DuLieuController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Model.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +11,30 @@ namespace KnowledgeStore.Areas.AdminArea.Controllers
     public class DuLieuController : Controller
     {
         // GET: AdminArea/DuLieu
+        KnowledgeStoreEntities db = new KnowledgeStoreEntities();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult BackUpData(string TenSaoLuu)
+        {
+            //SqlConnection sqlConnection = db.Database.Connection.Open();
+            //string exec = "use KnowledgeStore exec BackUpDatabaseBUD";
+            //SqlCommand sqlCommand = new SqlCommand(exec, );
+            //sqlCommand.ExecuteNonQuery();
+            //sqlConnection.Close();
+            //return View("Index");
+
+            using (SqlConnection con = new SqlConnection("data source=.;initial catalog=KnowledgeStore;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = @"use KnowledgeStore exec BackUpDatabaseBUD '" + TenSaoLuu + "'" ;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return View("Index");
         }
     }
 }
